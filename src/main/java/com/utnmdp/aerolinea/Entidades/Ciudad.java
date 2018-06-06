36950f1e2f3ec1e7acb1a1d87b9304922e10c63c
LAPTOP-15V47R3F\Usuario <leandrojesusljdmdq@gmail.com>
2018-06-06 20:07:43 -0300

    toma 3

--- src/main/java/com/utnmdp/aerolinea/Entidades/Ciudad.java
+++ src/main/java/com/utnmdp/aerolinea/Entidades/Ciudad.java
@@ -1,25 +1,28 @@
 package com.utnmdp.aerolinea.Entidades;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Ciudad
 {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_ciudad")
     private Integer id_ciudad;
 
+    @Column(name = "nombre_ciudad", nullable = false, length = 150)
     private String nombre_ciudad;
 
+    @Column(name = "codigo_iata", nullable = false, length = 3, unique = true)
+    private String codigo_iata;
 
+    public String getCodigo_iata() { return codigo_iata; }
+
+    public void setCodigo_iata(String codigo_iata) { this.codigo_iata = codigo_iata; }
 
     public String getNombre_ciudad() {
         return nombre_ciudad;
     }
-
 
     public void setNombre_ciudad(String nombre_ciudad) {
         this.nombre_ciudad = nombre_ciudad;
